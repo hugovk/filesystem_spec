0cb0d7a73fa04c44f9c1b44ca453c6fc01138ce4
Hugo van Kemenade <hugovk@users.noreply.github.com>
2023-08-28 19:36:12 +0300

    Add support for Python 3.12

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     description="File-system specification",
     long_description=long_description,
